dd1ce6b409e9d467acae2f45f64731e058518c2e
Abhimanyu <abhimanyukumar8787@gmail.com>
2023-06-10 23:35:20 +0530

    Added assert statement for selected location

--- src/test/java/appium/bookmyshow/tests/Select_Location_Functionality_Validations.java
+++ src/test/java/appium/bookmyshow/tests/Select_Location_Functionality_Validations.java
@@ -1,21 +1,24 @@
 package appium.bookmyshow.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import appium.bookmyshow.pages.HomePage;
 import appium.bookmyshow.pages.SelectLocationPage;
 import appium.utils.AndroidBaseTest;
 
 public class Select_Location_Functionality_Validations extends AndroidBaseTest{
     
     public SelectLocationPage selectLocationPage;
+    public HomePage homePage;
     
     @Test
     public void validate_select_location_functionality() throws Exception {
         selectLocationPage = signupPage.clickSkipButton();
         selectLocationPage.clickSelectManually();
         selectLocationPage.selectCityManually("Delhi");
-        selectLocationPage.selectRegionByIndex(1);
-        
+        homePage = selectLocationPage.selectRegionByIndex(1);
+        Assert.assertEquals(homePage.getSelectedLocation(), "Delhi");
     }
 
 }
