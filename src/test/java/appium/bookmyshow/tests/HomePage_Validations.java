1d53cef1dbcb26b10fa9c71bb9ede570ecb51166
Abhimanyu <abhimanyukumar8787@gmail.com>
2023-06-30 16:10:02 +0530

    Added Testcase to book movie ticket

--- src/test/java/appium/bookmyshow/tests/HomePage_Validations.java
+++ src/test/java/appium/bookmyshow/tests/HomePage_Validations.java
@@ -3,6 +3,7 @@ package appium.bookmyshow.tests;
 import org.testng.annotations.Test;
 
 import appium.bookmyshow.pages.HomePage;
+import appium.bookmyshow.pages.MoviesPage;
 import appium.bookmyshow.pages.SelectLocationPage;
 import appium.utils.AndroidBaseTest;
 
@@ -10,6 +11,7 @@ public class HomePage_Validations extends AndroidBaseTest{
     
     public SelectLocationPage selectLocationPage;
     public HomePage homePage;
+    public MoviesPage moviesPage;
     
     @Test
     public void book_movie_tickets_recommended_section() throws Exception {
@@ -18,6 +20,19 @@ public class HomePage_Validations extends AndroidBaseTest{
         selectLocationPage.selectCityManually("Delhi");
         homePage = selectLocationPage.selectRegionByIndex(1);
         homePage.selectMovieFromRecommended();
+    }
+    
+    @Test
+    public void book_movie_tickets() throws Exception {
+        selectLocationPage = signupPage.clickSkipButton();
+        selectLocationPage.clickSelectManually();
+        selectLocationPage.selectCityManually("Delhi");
+        homePage = selectLocationPage.selectRegionByIndex(1);
+        moviesPage = homePage.selectMoviesFromNavBar();
+        moviesPage.selectLanguage("English");
+        moviesPage.selectMovieByIndex(0);
+        moviesPage.bookTicket("IMAX", 2);
+        Thread.sleep(5000);
     }
 
 }
