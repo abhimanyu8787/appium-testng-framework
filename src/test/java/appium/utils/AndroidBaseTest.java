8c2598038bad99cde06250cb7f68e7f8dda84d36
Abhimanyu <abhimanyukumar8787@gmail.com>
2023-06-30 16:10:12 +0530

    added teardown afterclass method

--- src/test/java/appium/utils/AndroidBaseTest.java
+++ src/test/java/appium/utils/AndroidBaseTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.time.Duration;
 
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import appium.bookmyshow.pages.SignupPage;
@@ -13,25 +14,29 @@ import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 
 public class AndroidBaseTest {
-    
+
     public AndroidDriver driver;
     public AppiumDriverLocalService service;
     public FormPage formPage;
     public SignupPage signupPage;
-    
-    @BeforeClass(alwaysRun=true)
+
+    @BeforeClass(alwaysRun = true)
     public void ConfigureAppium() throws IOException, InterruptedException {
         String ipAddress = Utility.getGlobalProperties("IPAddress");
         String port = Utility.getGlobalProperties("Port");
         UiAutomator2Options options = new UiAutomator2Options();
         options.setDeviceName(Utility.getGlobalProperties("DeviceName"));
-        options.setApp(System.getProperty("user.dir")+"\\src\\test\\resources\\test-app\\bookmyshow.apk");
-        driver = new AndroidDriver(new URL (Utility.getGlobalProperties("AppiumServiceURL")), options);
+        options.setApp(System.getProperty("user.dir") + "\\src\\test\\resources\\test-app\\bookmyshow.apk");
+        driver = new AndroidDriver(new URL(Utility.getGlobalProperties("AppiumServiceURL")), options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        
-        //formPage = new FormPage(driver);
+
+        // formPage = new FormPage(driver);
         signupPage = new SignupPage(driver);
     }
     
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
 
 }
