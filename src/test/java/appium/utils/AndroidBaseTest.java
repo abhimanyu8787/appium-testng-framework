fbeb3efd8217c06db71ca8a84cd73962b671a960
Abhimanyu Kumar <abhimanyukumar8787@gmail.com>
2023-07-09 23:09:39 +0530

    Modified to include multiple apps
    Added modularity to select which app to be tested from global properties

--- src/test/java/appium/utils/AndroidBaseTest.java
+++ src/test/java/appium/utils/AndroidBaseTest.java
@@ -9,34 +9,38 @@ import org.testng.annotations.BeforeClass;
 
 import appium.bookmyshow.pages.SignupPage;
 import appium.ecommerce.pages.FormPage;
+import appium.goibibo.screens.LoginScreen;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 
 public class AndroidBaseTest {
 
-    public AndroidDriver driver;
-    public AppiumDriverLocalService service;
-    public FormPage formPage;
-    public SignupPage signupPage;
+	public AndroidDriver driver;
+	public AppiumDriverLocalService service;
+	public FormPage formPage;
+	public SignupPage signupPage;
+	public LoginScreen loginScreen;
 
-    @BeforeClass(alwaysRun = true)
-    public void ConfigureAppium() throws IOException, InterruptedException {
-        String ipAddress = Utility.getGlobalProperties("IPAddress");
-        String port = Utility.getGlobalProperties("Port");
-        UiAutomator2Options options = new UiAutomator2Options();
-        options.setDeviceName(Utility.getGlobalProperties("DeviceName"));
-        options.setApp(System.getProperty("user.dir") + "\\src\\test\\resources\\test-app\\bookmyshow.apk");
-        driver = new AndroidDriver(new URL(Utility.getGlobalProperties("AppiumServiceURL")), options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	@BeforeClass(alwaysRun = true)
+	public void ConfigureAppium() throws IOException, InterruptedException {
+		String ipAddress = Utility.getGlobalProperties("IPAddress");
+		String port = Utility.getGlobalProperties("Port");
+		UiAutomator2Options options = new UiAutomator2Options();
+		options.setDeviceName(Utility.getGlobalProperties("DeviceName"));
+		options.setApp(System.getProperty("user.dir") + "\\src\\test\\resources\\test-app\\goibibo.apk");
+		driver = new AndroidDriver(new URL(Utility.getGlobalProperties("AppiumServiceURL")), options);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
-        // formPage = new FormPage(driver);
-        signupPage = new SignupPage(driver);
-    }
-    
-    @AfterClass
-    public void tearDown() {
-        driver.quit();
-    }
+		String appName = Utility.getGlobalProperties("App");
+		if(appName.equalsIgnoreCase("goibibo")) {
+			loginScreen =  new LoginScreen(driver);
+		}
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
 
 }
